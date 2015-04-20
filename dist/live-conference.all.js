3dc05ca5aec78e7f5db6bd9bd39156c2f4439c4e
Philipp Kewisch <pkewisch@linagora.com>
2015-04-20 09:40:06 +0200

    [MEET-290] - Handle reconnection

--- dist/live-conference.all.js
+++ dist/live-conference.all.js
@@ -1030,7 +1030,7 @@ angular.module('op.live-conference')
         easyrtc.call(otherEasyrtcid, onSuccess, onFailure);
       }
 
-      function connect(conferenceState) {
+      function connect(conferenceState, callback) {
 
         function entryListener(entry, roomName) {
           if (entry) {
@@ -1071,14 +1071,16 @@ angular.module('op.live-conference')
         easyrtc.setRoomEntryListener(entryListener);
 
         var conference = conferenceState.conference;
-        easyrtc.joinRoom(conference._id, null,
-          function() {
-            $log.debug('Joined room ' + conference._id);
-          },
-          function() {
-            $log.debug('Error while joining room ' + conference._id);
-          }
-        );
+        if (!(conference._id in easyrtc.roomJoin)) {
+          easyrtc.joinRoom(conference._id, null,
+            function() {
+              $log.debug('Joined room ' + conference._id);
+            },
+            function() {
+              $log.debug('Error while joining room ' + conference._id);
+            }
+          );
+        }
 
         easyrtc.username = session.getUserId();
 
@@ -1098,10 +1100,16 @@ angular.module('op.live-conference')
             if (!videoEnabled) {
               conferenceState.updateMuteVideoFromIndex(0, true);
             }
+            if (callback) {
+              callback(null);
+            }
           }
 
           function onLoginFailure(errorCode, message) {
             $log.error('Error while connecting to the webrtc signaling service ' + errorCode + ' : ' + message);
+            if (callback) {
+              callback(errorCode);
+            }
           }
 
           easyrtc.setOnError(function(errorObject){
