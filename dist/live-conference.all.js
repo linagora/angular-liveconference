4e0c22b167ec325b1cfc74898d0dc525ba0e36de
Romain PIGNOLET <rpignolet@linagora.com>
2015-04-21 18:31:29 +0200

    MEET-307 Compile stuff

--- dist/live-conference.all.js
+++ dist/live-conference.all.js
@@ -520,8 +520,8 @@ angular.module('op.live-conference')
       }
     };
   })
-  .directive('scaleToCanvas', ['$interval', '$window', 'cropDimensions', 'drawAvatarIfVideoMuted', '$log',
-    function($interval, $window, cropDimensions, drawAvatarIfVideoMuted, $log) {
+  .directive('scaleToCanvas', ['$interval', '$window', 'cropDimensions', 'drawAvatarIfVideoMuted', 'drawHelper',
+    function($interval, $window, cropDimensions, drawAvatarIfVideoMuted, drawHelper) {
 
     var requestAnimationFrame =
       $window.requestAnimationFrame ||
@@ -551,7 +551,7 @@ angular.module('op.live-conference')
         drawAvatarIfVideoMuted(vid.id, ctx, width, height, function() {
           var cropDims = cropDimensions(width, height, vWidth, vHeight);
 
-          ctx.drawImage(vid, cropDims[0], cropDims[1], cropDims[2], cropDims[2], 0, 0, width, height);
+          drawHelper.drawImage(ctx, vid, cropDims[0], cropDims[1], cropDims[2], cropDims[2], 0, 0, width, height);
         });
       }
 
@@ -821,7 +821,33 @@ angular.module('op.live-conference')
 
 angular.module('op.live-conference')
 
-  .factory('drawVideo', ['$window', '$interval', 'drawAvatarIfVideoMuted', function($window, $interval, drawAvatarIfVideoMuted) {
+  .factory('drawHelper', function () {
+
+    function drawImage(context) {
+      // see https://bugzilla.mozilla.org/show_bug.cgi?id=879717
+      // Sometimes Firefox drawImage before it is even available.
+      // Thus we ignore this error.
+
+      var argumentsArray = [];
+      for (var i = 1 ; i < arguments.length ; i++) {
+        argumentsArray.push(arguments[i]);
+      }
+
+      try {
+        context.drawImage.apply(context, argumentsArray);
+      } catch (e) {
+        if (e.name !== 'NS_ERROR_NOT_AVAILABLE') {
+          throw e;
+        }
+      }
+    }
+
+    return {
+      drawImage: drawImage
+    };
+  })
+
+  .factory('drawVideo', ['$window', '$interval', 'drawAvatarIfVideoMuted', 'drawHelper', function($window, $interval, drawAvatarIfVideoMuted, drawHelper) {
     var requestAnimationFrame =
       $window.requestAnimationFrame ||
       $window.mozRequestAnimationFrame ||
@@ -832,19 +858,9 @@ angular.module('op.live-conference')
     var promise;
 
     function draw(context, video, width, height) {
-      // see https://bugzilla.mozilla.org/show_bug.cgi?id=879717
-      // Sometimes Firefox drawImage before it is even available.
-      // Thus we ignore this error.
-      try {
-        drawAvatarIfVideoMuted(video.id, context, width, height, function() {
-          context.drawImage(video, 0, 0, width, height);
-        });
-      } catch (e) {
-        if (e.name !== 'NS_ERROR_NOT_AVAILABLE') {
-          throw e;
-        }
-      }
-
+      drawAvatarIfVideoMuted(video.id, context, width, height, function() {
+        drawHelper.drawImage(context, video, 0, 0, width, height);
+      });
     }
 
     return function(context, video, width, height) {
@@ -920,8 +936,8 @@ angular.module('op.live-conference')
     };
   })
 
-  .factory('drawAvatarIfVideoMuted', ['currentConferenceState', 'attendeeColorsService', 'getCoordinatesOfCenteredImage', '$log',
-    function(currentConferenceState, attendeeColorsService, getCoordinatesOfCenteredImage, $log) {
+  .factory('drawAvatarIfVideoMuted', ['currentConferenceState', 'attendeeColorsService', 'getCoordinatesOfCenteredImage', '$log', 'drawHelper',
+    function(currentConferenceState, attendeeColorsService, getCoordinatesOfCenteredImage, $log, drawHelper) {
     return function(videoId, context, width, height, otherwise) {
       var attendee = currentConferenceState.getAttendeeByVideoId(videoId);
 
@@ -950,7 +966,7 @@ angular.module('op.live-conference')
           context.clearRect(0, 0, width, height);
           context.fillStyle = attendeeColorsService.getColorForAttendeeAtIndex(attendee.index);
           context.fillRect(coords.x, coords.y, coords.size, coords.size);
-          context.drawImage(image, coords.x, coords.y, coords.size, coords.size);
+          drawHelper.drawImage(context, image, coords.x, coords.y, coords.size, coords.size);
 
           canvas.drawnAvatarVideoId = videoId;
         });
