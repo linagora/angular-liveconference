281699e2a3b1712a80487ad8c08ad0f379ecb68f
Corentin STG_CADIOU <ccadiou@linagora.com>
2015-06-18 17:15:31 +0200

    MEET-399 error in attendee attribute

--- dist/live-conference.all.js
+++ dist/live-conference.all.js
@@ -1569,7 +1569,7 @@ angular.module('op.live-conference')
       function shoot(screenshotEdgePx) {
         var attendee = currentConferenceState.getAttendeeByVideoId(LOCAL_VIDEO_ID);
 
-        if (!attendee || attendee.videoMuted) {
+        if (!attendee || attendee.muteVideo) {
           return null;
         }
 
