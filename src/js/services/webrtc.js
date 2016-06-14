0fbb0762ee51da68d7d72068fbec08ece1dd3720
RockyMeadow <dtpham@linagora.com>
2016-06-14 11:40:52 +0700

    MEET-521 Modify angular-liveconference to use a frontend webrtc adapter

---
+++ src/js/services/webrtc.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('op.live-conference')
+  .factory('webRTCService', ['easyRTCAdapter', function(easyRTCAdapter) {
+    var adapter = easyRTCAdapter;
+
+    return {
+        leaveRoom: adapter.leaveRoom,
+        performCall: adapter.performCall,   
+        connect: adapter.connect,
+        canEnumerateDevices: adapter.canEnumerateDevices,
+        enableMicrophone: adapter.enableMicrophone,
+        muteRemoteMicrophone: adapter.muteRemoteMicrophone,
+        enableCamera: adapter.enableCamera,
+        enableVideo: adapter.enableVideo,
+        isVideoEnabled: adapter.isVideoEnabled,
+        configureBandwidth: adapter.configureBandwidth,
+        setPeerListener: adapter.setPeerListener,
+        myEasyrtcid: adapter.myEasyrtcid,
+        broadcastData: adapter.broadcastData,
+        broadcastMe: adapter.broadcastMe,
+        addDisconnectCallback: adapter.addDisconnectCallback,
+        removeDisconnectCallback: adapter.removeDisconnectCallback,
+        sendDataP2P: adapter.sendDataP2P,
+        sendDataWS: adapter.sendDataWS,
+        sendData: adapter.sendData,
+        getP2PConnectionStatus: adapter.getP2PConnectionStatus,
+        doesDataChannelWork: adapter.doesDataChannelWork,
+        setGotMedia: adapter.setGotMedia,
+        NOT_CONNECTED: adapter.NOT_CONNECTED,
+        BECOMING_CONNECTED: adapter.BECOMING_CONNECTED,
+        IS_CONNECTED: adapter.IS_CONNECTED,
+        addDataChannelOpenListener: adapter.addDataChannelOpenListener,
+        addDataChannelCloseListener: adapter.addDataChannelCloseListener,
+        removeDataChannelOpenListener: adapter.removeDataChannelOpenListener,
+        removeDataChannelCloseListener: adapter.removeDataChannelCloseListener,
+        addPeerListener: adapter.addPeerListener,
+        removePeerListener: adapter.removePeerListener,
+        connection: connection,
+        getOpenedDataChannels: adapter.getOpenedDataChannels
+    };
+  }]);
