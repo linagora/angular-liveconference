8e3706289a001c7214f9825b6a489e5ca50c3707
Christophe Hamerling <chamerling@linagora.com>
2018-01-12 11:49:43 +0100

    linagora/lgs/openpaas/hublin#526 Add registry for frontend connector

--- src/js/services/webrtc.js
+++ src/js/services/webrtc.js
@@ -1,42 +1,49 @@
-'use strict';
+(function(angular) {
 
-angular.module('op.live-conference')
-  .factory('webRTCService', ['easyRTCAdapter', function(easyRTCAdapter) {
-    var adapter = easyRTCAdapter;
+  'use strict';
+
+  angular.module('op.live-conference')
+    .factory('webRTCService', webRTCService);
+
+  function webRTCService(webRTCAdapterRegistry) {
+    // TODO: Use config from somewhere to get activated one...
+    var adapter = webRTCAdapterRegistry.get('janus');
 
     return {
-        leaveRoom: adapter.leaveRoom,
-        performCall: adapter.performCall,
-        connect: adapter.connect,
-        canEnumerateDevices: adapter.canEnumerateDevices,
-        enableMicrophone: adapter.enableMicrophone,
-        muteRemoteMicrophone: adapter.muteRemoteMicrophone,
-        enableCamera: adapter.enableCamera,
-        enableVideo: adapter.enableVideo,
-        isVideoEnabled: adapter.isVideoEnabled,
-        configureBandwidth: adapter.configureBandwidth,
-        setPeerListener: adapter.setPeerListener,
-        myRtcid: adapter.myRtcid,
-        broadcastData: adapter.broadcastData,
-        broadcastMe: adapter.broadcastMe,
-        addDisconnectCallback: adapter.addDisconnectCallback,
-        removeDisconnectCallback: adapter.removeDisconnectCallback,
-        sendDataP2P: adapter.sendDataP2P,
-        sendDataWS: adapter.sendDataWS,
-        sendData: adapter.sendData,
-        getP2PConnectionStatus: adapter.getP2PConnectionStatus,
-        doesDataChannelWork: adapter.doesDataChannelWork,
-        setGotMedia: adapter.setGotMedia,
-        NOT_CONNECTED: adapter.NOT_CONNECTED,
-        BECOMING_CONNECTED: adapter.BECOMING_CONNECTED,
-        IS_CONNECTED: adapter.IS_CONNECTED,
-        addDataChannelOpenListener: adapter.addDataChannelOpenListener,
-        addDataChannelCloseListener: adapter.addDataChannelCloseListener,
-        removeDataChannelOpenListener: adapter.removeDataChannelOpenListener,
-        removeDataChannelCloseListener: adapter.removeDataChannelCloseListener,
-        addPeerListener: adapter.addPeerListener,
-        removePeerListener: adapter.removePeerListener,
-        connection: adapter.connection,
-        getOpenedDataChannels: adapter.getOpenedDataChannels
+      leaveRoom: adapter.leaveRoom,
+      performCall: adapter.performCall,
+      connect: adapter.connect,
+      canEnumerateDevices: adapter.canEnumerateDevices,
+      enableMicrophone: adapter.enableMicrophone,
+      muteRemoteMicrophone: adapter.muteRemoteMicrophone,
+      enableCamera: adapter.enableCamera,
+      enableVideo: adapter.enableVideo,
+      isVideoEnabled: adapter.isVideoEnabled,
+      configureBandwidth: adapter.configureBandwidth,
+      setPeerListener: adapter.setPeerListener,
+      myRtcid: adapter.myRtcid,
+      broadcastData: adapter.broadcastData,
+      broadcastMe: adapter.broadcastMe,
+      addDisconnectCallback: adapter.addDisconnectCallback,
+      removeDisconnectCallback: adapter.removeDisconnectCallback,
+      sendDataP2P: adapter.sendDataP2P,
+      sendDataWS: adapter.sendDataWS,
+      sendData: adapter.sendData,
+      getP2PConnectionStatus: adapter.getP2PConnectionStatus,
+      doesDataChannelWork: adapter.doesDataChannelWork,
+      setGotMedia: adapter.setGotMedia,
+      NOT_CONNECTED: adapter.NOT_CONNECTED,
+      BECOMING_CONNECTED: adapter.BECOMING_CONNECTED,
+      IS_CONNECTED: adapter.IS_CONNECTED,
+      addDataChannelOpenListener: adapter.addDataChannelOpenListener,
+      addDataChannelCloseListener: adapter.addDataChannelCloseListener,
+      removeDataChannelOpenListener: adapter.removeDataChannelOpenListener,
+      removeDataChannelCloseListener: adapter.removeDataChannelCloseListener,
+      addPeerListener: adapter.addPeerListener,
+      removePeerListener: adapter.removePeerListener,
+      connection: adapter.connection,
+      getOpenedDataChannels: adapter.getOpenedDataChannels
     };
-  }]);
+  }
+})(angular);
+
